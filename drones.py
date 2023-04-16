17dc8d4faff93b6a23999cdc51a472f571780f48
Sahil Nagaralu <92047320+SahilNDev@users.noreply.github.com>
2023-04-16 19:43:26 +0530

    Update drones.py

--- drones.py
+++ drones.py
@@ -18,11 +18,8 @@ st.title("Predict whether it is a bird or drone")
 
 file = st.file_uploader("Upload the image here:", type=["jpg"])
 if st.button("Submit"):
-    with(file.name, "wb") as img_file:
-        img_file.write(file.getbuffer())
-    bytes_data = file.name
-    st.image(bytes_data)
-    item = cv2.resize(bytes_data, (100,100))
+    st.image(file)
+    item = cv2.resize(file, (100,100))
     nsamples,nx,ny = item.shape
     tester = item.reshape((nsamples,nx*ny))
     model = pickle.load(open("model.sav", "rb"))
