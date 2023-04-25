e67ad901df3e97d166d71608656fdf6b970a8a29
Divyam-kr <122778685+Divyam-kr@users.noreply.github.com>
2023-04-26 01:23:43 +0530

    Update drones.py

--- drones.py
+++ drones.py
@@ -6,7 +6,7 @@ st.markdown(
          f"""
          <style>
          .stApp {{
-             background-image: url("https://github.com/aadarsh1810/Color_Detection_YOLO3/blob/main/black-background.gif?raw=true");
+             background-image: url("https://github.com/Divyam-kr/Files-for-streamlit-design-elements/blob/main/Drone.gif?raw=true");
              background-attachment: fixed;
              background-size: cover
          }}
