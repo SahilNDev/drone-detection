f1c3d6a8a5c09698fe0aef71ee9c2e4f24ee22e8
Sahil Nagaralu <92047320+SahilNDev@users.noreply.github.com>
2023-04-29 00:28:15 +0530

    Update drones.py

--- drones.py
+++ drones.py
@@ -23,16 +23,16 @@ with tab1:
          if st.button("Submit"):
            with open(file.name, "wb") as image:
              image.write(file.getbuffer())
-         bytes_data = file.name
-         st.image(bytes_data)
-         img_new = cv2.imread(bytes_data,0)
-         item = cv2.resize(img_new, (100,100))
-         nx,ny = item.shape
-         tester = item.reshape((1,nx*ny))
-         model = pickle.load(open("model.sav", "rb"))
-         prediction = model.predict(tester)
-         dict1 = {0:'Bird',1:'Drone'}
-         st.write(dict1[prediction[0]])
+             bytes_data = file.name
+             st.image(bytes_data)
+             img_new = cv2.imread(bytes_data,0)
+             item = cv2.resize(img_new, (100,100))
+             nx,ny = item.shape
+             tester = item.reshape((1,nx*ny))
+             model = pickle.load(open("model.sav", "rb"))
+             prediction = model.predict(tester)
+             dict1 = {0:'Bird',1:'Drone'}
+             st.write(dict1[prediction[0]])
          
 with tab2:
     st.header("About Us")
