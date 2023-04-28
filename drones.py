fc50ee6b0fe46e644ec581fec9a5a2434cb017b5
Divyam-kr <122778685+Divyam-kr@users.noreply.github.com>
2023-04-29 00:05:27 +0530

    Update drones.py

--- drones.py
+++ drones.py
@@ -14,20 +14,30 @@ st.markdown(
          """,
          unsafe_allow_html=True
      )
-
 st.title("Predict whether it is a bird or drone: ")
+with tab1:
+         st.title("Model")
 
-file = st.file_uploader("Upload the image here:", type=["jpg", "jpeg","png"])
-if st.button("Submit"):
-    with open(file.name, "wb") as image:
-         image.write(file.getbuffer())
-    bytes_data = file.name
-    st.image(bytes_data)
-    img_new = cv2.imread(bytes_data,0)
-    item = cv2.resize(img_new, (100,100))
-    nx,ny = item.shape
-    tester = item.reshape((1,nx*ny))
-    model = pickle.load(open("model.sav", "rb"))
-    prediction = model.predict(tester)
-    dict1 = {0:'Bird',1:'Drone'}
-    st.write(dict1[prediction[0]])
+         file = st.file_uploader("Upload the image here:", type=["jpg", "jpeg","png"])
+         if st.button("Submit"):
+           with open(file.name, "wb") as image:
+             image.write(file.getbuffer())
+         bytes_data = file.name
+         st.image(bytes_data)
+         img_new = cv2.imread(bytes_data,0)
+         item = cv2.resize(img_new, (100,100))
+         nx,ny = item.shape
+         tester = item.reshape((1,nx*ny))
+         model = pickle.load(open("model.sav", "rb"))
+         prediction = model.predict(tester)
+         dict1 = {0:'Bird',1:'Drone'}
+         st.write(dict1[prediction[0]])
+         
+with tab2:
+    st.header("About Us")
+    st.markdown("The Creators of the Website:")
+    st.write("1. Divyam Kumar")
+    st.write("2. Harsh Gandhi")
+    st.write("3. Dhrubo Bhattacharjee")
+    st.write("4. Jainil Patel")
+         
