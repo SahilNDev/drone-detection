c7d6063dc256444fea89caa6156846a4fdb0219b
Divyam-kr <122778685+Divyam-kr@users.noreply.github.com>
2023-04-28 20:23:11 +0530

    Update drones.py

--- drones.py
+++ drones.py
@@ -6,7 +6,7 @@ st.markdown(
          f"""
          <style>
          .stApp {{
-             background-image: url("https://github.com/Divyam-kr/Files-for-streamlit-design-elements/blob/main/dr_AdobeExpress.gif?raw=true");
+             background-image: url("https://github.com/Divyam-kr/Files-for-streamlit-design-elements/blob/main/ezgif.com-video-to-gif.gif?raw=true");
              background-attachment: fixed;
              background-size: cover
          }}
